680b4239e0663df386f88f663387971e7b19251a
root <natnaelgaredew@yahoo.com>
2022-10-03 08:01:57 -0700

    commit5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+
+/**
+ * main - print a sentence
+ *@void = Null value
+ * Return: 0
+ */
+
 int main(void)
 {
-	printf("\"with proper grammer, but the outcome is a piece of art");
-	return (value * (long long)value);
+	printf("with proper grammer, but the outcome is a piece of art");
+	return (0);
 }
