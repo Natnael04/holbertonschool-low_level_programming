0cf19cf551bd601b5a4d7fe27738f7c6b57906ed
root <natnaelgaredew@yahoo.com>
2022-10-05 06:54:20 -0700

    commit4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+	char n;
+
+	n = 'a';
+	while (n <= 'z')
+	{
+		if(n != ('q', 'e'));
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
