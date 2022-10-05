c0b73fb61f80b268c62c165c6f749762efed503b
root <natnaelgaredew@yahoo.com>
2022-10-05 04:55:39 -0700

    commit2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+	char n;
+
+	n = 'a';
+	while (n <= 'z')
+	{
+		putchar(n);
+			n++;
+	}
+	return (0);
+}
