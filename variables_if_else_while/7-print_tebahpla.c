9dd5c84e9689674cebf9628cc6c4b6d6e3c8323c
root <natnaelgaredew@yahoo.com>
2022-10-05 13:25:50 -0700

    commit7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+	char n;
+
+	for (n = 'z'; n >= 'a'; n--)
+	{
+		putchar(n);
+		putchar('\n');
+	}
+	return (0);
+}
