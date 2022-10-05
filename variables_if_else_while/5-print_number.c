5b11b7029f66c1c60f2553ce88fb36a16cffe7ca
root <natnaelgaredew@yahoo.com>
2022-10-05 08:41:18 -0700

    commit5

---
+++ variables_if_else_while/5-print_number.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n <= 9 ; n++)
+	{
+		printf("%d", n);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
