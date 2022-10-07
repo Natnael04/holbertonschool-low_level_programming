c63bff5eb2856050bf137a3ed5fb7ed046cef02c
root <natnaelgaredew@yahoo.com>
2022-10-07 05:27:19 -0700

    commit2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Print "lowercase alphabet 10 times"
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void);
+
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar (i);
+
+	}
+	_putchar ('\n');
+
+}
