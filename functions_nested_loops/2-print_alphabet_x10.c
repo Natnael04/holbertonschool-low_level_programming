c99a398225434c6d76a2c57796216d80e8c02ce9
root <natnaelgaredew@yahoo.com>
2022-10-07 07:10:33 -0700

    commit2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -5,16 +5,19 @@
  *
  * Return: Always 0.
  */
-void print_alphabet_x10(void);
-
+void print_alphabet_x10(void)
 {
-	char i;
+	char i, j;
+
+	for (j = 0; j < 10; i++)
+	{
 
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar (i);
-
+	}
 	}
 	_putchar ('\n');
+
 
 }
