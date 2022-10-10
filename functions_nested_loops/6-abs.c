7e64cbac4c1748bcb556e5d4a2eeade7d6dcc777
root <natnaelgaredew@yahoo.com>
2022-10-10 07:32:25 -0700

    commit5

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * abs - computes the value of an integer
+ * @: character to check
+ * Return: Always 0
+ */
+int _abs(int)
+{
+	int i;
+
+	if(i < 0)
+	{
+		i = i * (-1);
+	}
+	return (0);
+}
