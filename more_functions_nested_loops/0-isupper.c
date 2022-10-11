2ec24bb75458ceb682c417e1abc40194f9400bfd
root <natnaelgaredew@yahoo.com>
2022-10-11 09:10:37 -0700

    commit1

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+* _isupper - to check for uppercase character.
+ *@c : int parameter
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+{
+
+	if (c == 'A')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
