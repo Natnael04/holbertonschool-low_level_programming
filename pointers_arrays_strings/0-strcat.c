2e70a42b2843b5f02c23ed8aaa654675be851721
root <natnaelgaredew@yahoo.com>
2022-10-15 12:01:32 -0700

    commit01

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -13,13 +13,17 @@ char *_strcat(char *dest, char *src)
 {
 	int i;
 
-	int j = strlen(src);
+	int j = strlen(src) - 1;
+
+	for (j = 0; dest[j] != '\0'; j++)
+	{
 
 	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[i + j] = src[i];
 	}
 	dest[i + j] = '\0';
+	}
 	return (dest);
 
 }
