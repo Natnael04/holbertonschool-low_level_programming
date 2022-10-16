aafc564caeb7fce47b35c2c95457e4616a362f3b
root <natnaelgaredew@yahoo.com>
2022-10-16 04:20:10 -0700

    commit5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,4 @@
-#include"main.h"
-#include"string.h"
+#include "holberton.h"
 
 /**
  * rev_string - Reverses a string
@@ -9,16 +8,19 @@
  */
 void rev_string(char *s)
 {
-	int i = 0;
-	int c = 0;
-	char *a;
-	char aux = s[i];
+	int i, c, k;
+	char *a, aux;
 
 	a = s;
 
 	while (s[c] != '\0')
 	{
 		c++;
+	}
+
+	for (k = 1; k < c; k++)
+	{
+		a++;
 	}
 
 	for (i = 0; i < (c / 2); i++)
