6ca75e2408bdc9e70bb85959292b81621583a3a8
root <natnaelgaredew@yahoo.com>
2022-10-16 03:09:35 -0700

    commit5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -9,14 +9,16 @@
  */
 void rev_string(char *s)
 {
-	int i, c;
+	int i = 0;
+	int c = 0;
 	char *a;
-	+
+	char aux = s[i];
+
 	a = s;
 
 	while (s[c] != '\0')
 	{
-	       
+		c++;
 	}
 
 	for (i = 0; i < (c / 2); i++)
