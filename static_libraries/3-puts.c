76f3295bd7ba0693c56561d2e8d4c6db8c71358c
root <natnaelgaredew@yahoo.com>
2022-10-21 06:22:11 -0700

    commit00

---
+++ static_libraries/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - to print a string
+ *@str : a string to print
+ *
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
---
+++ static_libraries/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - computes the value of an integer
+ * @i: character to check
+ * Return: value of an integer
+ */
+int _abs(int i)
+{
+
+	if (i < 0)
+	{
+		i = i * (-1);
+	}
+	return (i);
+
+}
