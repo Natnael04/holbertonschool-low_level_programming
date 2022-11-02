0efbc58abe4194f7349e2eee7f6e45140cbcf19e
root <natnaelgaredew@yahoo.com>
2022-11-02 06:28:37 -0700

    commit00

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - A new type describe a dog
+ * @name: the name of a dog
+ * @age: the age of a dog
+ * @owner: the owner of a dog
+ */
 struct dog
 {
 	char *name;
