5113f968da4ae05e03f46745c9970cd7339a7a85
root <natnaelgaredew@yahoo.com>
2022-11-02 03:24:58 -0700

    commit01

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - to initialize a variable of type
+ * @d: to initialize the dog
+ * @name: the name of dog
+ * @age: the age of dog
+ * @owner: the owner of dog
+ *
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
