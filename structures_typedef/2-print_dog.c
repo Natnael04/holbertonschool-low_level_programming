a463b972c01cd09a0851848c961fc4263196bdd5
root <natnaelgaredew@yahoo.com>
2022-11-07 01:31:25 -0800

    commit02

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 
 /**
  * print_dog - to print a variable type struct dog
  * @d: to print a struct dog
  *
- * Return: Always 0.
  */
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
+
 	if (d->name == NULL)
 		printf("Name: (nil)\n");
 	else
