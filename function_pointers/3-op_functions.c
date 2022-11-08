4599039a4caf1c2611f436ca55f570352234381e
root <natnaelgaredew@yahoo.com>
2022-11-08 06:26:25 -0800

    commit04

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,55 @@
+/**
+ * op_add - to add two integer
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - to subtract two integer
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - to multiply two integer
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - to divide two number
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: quotient
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - to find the reminder of the division result
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: reminder
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
