b556c683381d643e84927fd70408c39c1299a702
root <natnaelgaredew@yahoo.com>
2022-11-16 01:17:30 -0800

    commit01

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - show the number of elements of a list
+ * @h: a linked list
+ *
+ *Return: the number of elements in a linked list_t list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
