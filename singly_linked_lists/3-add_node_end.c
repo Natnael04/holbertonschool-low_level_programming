2b3b0ce6376cd632fd6dfdc2f642873ad43df663
root <natnaelgaredew@yahoo.com>
2022-11-17 01:04:52 -0800

    commit03

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -12,30 +12,34 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new, *temp;
+	char *dup;
+	int len;
+	list_t *new, *last;
 
-	new = (list_t *)malloc(sizeof(list_t));
-	if (!new)
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	dup = strdup(str);
+	if (str == NULL)
 	{
+		free(new);
 		return (NULL);
 	}
-	new->str = strdup(str);
-	new->len = strlen(str);
+	for (len = 0; str[len];)
+		len++;
+	new->str = dup;
+	new->len = len;
+	new->next = NULL;
 
-	while (temp->next)
+	if (*head == NULL)
+		*head = new;
+	else
 	{
-		temp = temp->next;
-		if (*head == NULL)
-		{
-		*head = temp = new;
-		}
-		else
-		{
-		temp = *head;
-		temp->next = new;
-		temp = new;
-		}
+		last = *head;
+		while (last->next != NULL)
+			last = last->next;
+		last->next = new;
 	}
-		return (temp);
-
+	return (*head);
 }
