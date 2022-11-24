3f81cf5c8b01e8e7b26b4b31a13a76a883337e73
root <natnaelgaredew@yahoo.com>
2022-11-24 06:28:39 -0800

    commit01

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -18,7 +18,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(my_list, n);
 	for (counter = 0; counter < n; counter++)
 	{
-		printf("%d", counter);
+		printf("%d", va_arg(my_list, int));
 
 		if (counter != n - 1)
 			printf("%s", separator);
