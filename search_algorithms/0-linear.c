5b757a6e885e33eb6c82657119b93ec28f2298b6
root <natnaelgaredew@yahoo.com>
2023-01-04 06:38:11 -0800

    commit00

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+/**
+ * linear_serach -serach for a value in an array of integer
+ * @array: a pointer to the first element
+ * @size: the number of element in array
+ * @value: the value to search for
+ *
+ * Return: the first index and if it is not present in array NULL
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	int i;
+
+	for (i = 0; i < ((int)size); i++)
+	{
+		if (array[i] == value)
+			return i;
+
+	}
+	return -1;
+}
