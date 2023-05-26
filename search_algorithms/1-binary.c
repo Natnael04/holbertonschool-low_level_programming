abc167de54d55beb386b3c5fecc7ed797252faad
Natnael04 <natnaeldeboch@gmail.com>
2023-05-26 01:14:41 -0700

    mod

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -9,21 +9,38 @@
  *
  * Return: the index or -1 if the array is not present
  */
-int binary_search(int *array, size_t size, int value)
-{
-	int i = 0;
-	int j = size - 1;
-	int mid;
+int binary_search(int *array, size_t size, int value) {
+    int left = 0;
+    int right = size - 1;
+    int mid;
 
-	while (i < j)
+    if (array == NULL) {
+        return -1;
+    }
+
+    
+
+    while (left <= right)
 	{
-		mid = ((i + j) / 2);
-		if (value == array[mid])
-			return (mid);
-		else if (value < array[mid])
-			j = (mid - 1);
-		else
-			j = (mid + 1);
-	}
-	return (-1);
+        mid = left + (right - left) / 2;
+
+        printf("Searching in subarray: ");
+        {
+            int i;
+            for (i = left; i <= right; i++){
+            printf("%d ", array[i]);}
+        }
+        printf("\n");
+
+        if (array[mid] == value) {
+            return mid;
+        } else if (array[mid] < value) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return -1;
 }
+
